3d8d92d8974300fa5088fb1be97265517eec6c92
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2022-12-13 14:25:23 +0100

    Refactors to the default executor

--- src/main/java/dev/costas/minicli/processors/DefaultCommandExecutor.java
+++ src/main/java/dev/costas/minicli/processors/DefaultCommandExecutor.java
@@ -2,9 +2,7 @@ package dev.costas.minicli.processors;
 
 import dev.costas.minicli.RunnableCommand;
 import dev.costas.minicli.annotation.Flag;
-import dev.costas.minicli.annotation.OnInvoke;
 import dev.costas.minicli.annotation.Parameter;
-import dev.costas.minicli.models.CommandOutput;
 import dev.costas.minicli.models.Invocation;
 
 import java.io.PrintStream;
@@ -22,47 +20,60 @@ public class DefaultCommandExecutor implements CommandExecutor {
 	 * The output of the command is printed to the standard output.
 	 *
 	 * @param instance The command instance.
-	 * @param args The invocation of the command.
+	 * @param args     The invocation of the command.
 	 */
 	public void execute(RunnableCommand instance, Invocation args, PrintStream out) {
 		try {
-			var fields = instance.getClass().getDeclaredFields();
-			for (var field : fields) {
-				var annotations = field.getAnnotations();
-				if (annotations.length != 1) {
-					continue;
-				}
-
-				if (annotations[0] instanceof Flag param) {
-					var value = args.getFlag(param.name());
-					if (value == null) {
-						value = args.getFlag(param.shortName());
-						if (value == null) {
-							value = param.defaultValue();
-						}
-					}
-					field.setAccessible(true);
-					field.set(instance, value);
-				} else if (annotations[0] instanceof Parameter param) {
-					var value = args.getParameter(param.name());
-					if (value == null) {
-						value = args.getParameter(param.shortName());
-						if (value == null) {
-							if (param.required()) {
-								throw new RuntimeException("Missing required parameter: " + param.name());
-							}
-							value = param.defaultValue();
-						}
-					}
-					field.setAccessible(true);
-					field.set(instance, value);
-				}
-			}
+			injectArguments(instance, args);
 
 			var result = instance.run();
 			out.println(result.output());
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	private void injectArguments(RunnableCommand instance, Invocation args) throws IllegalAccessException {
+		var fields = instance.getClass().getDeclaredFields();
+		for (var field : fields) {
+			var annotations = field.getAnnotations();
+			if (annotations.length != 1) {
+				continue;
+			}
+
+			var annotation = Arrays.stream(annotations)
+					.filter(a -> a instanceof Parameter || a instanceof Flag)
+					.findFirst()
+					.orElse(null);
+
+			if (annotation == null) {
+				continue;
+			}
+
+			if (annotation instanceof Flag flag) {
+				var value = args.getFlag(flag.name());
+				if (value == null) {
+					value = args.getFlag(flag.shortName());
+					if (value == null) {
+						value = flag.defaultValue();
+					}
+				}
+				field.setAccessible(true);
+				field.set(instance, value);
+			} else if (annotation instanceof Parameter param) {
+				var value = args.getParameter(param.name());
+				if (value == null) {
+					value = args.getParameter(param.shortName());
+					if (value == null) {
+						if (param.required()) {
+							throw new RuntimeException("Missing required parameter: " + param.name());
+						}
+						value = param.defaultValue();
+					}
+				}
+				field.setAccessible(true);
+				field.set(instance, value);
+			}
 		}
 	}
 }
