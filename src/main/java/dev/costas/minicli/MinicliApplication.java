0d7b7258967ab2c8080b64dd587dc969a9f8f367
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2022-12-13 19:16:28 +0100

    Refactor argument injection to support injecting String, Long, Int, Double and Float (and primitive equivalents)
    References #2

--- src/main/java/dev/costas/minicli/MinicliApplication.java
+++ src/main/java/dev/costas/minicli/MinicliApplication.java
@@ -1,15 +1,19 @@
 package dev.costas.minicli;
 
 import dev.costas.minicli.annotation.Command;
+import dev.costas.minicli.annotation.Flag;
+import dev.costas.minicli.annotation.Parameter;
 import dev.costas.minicli.exceptions.QuitException;
 import dev.costas.minicli.framework.ArgumentParser;
 import dev.costas.minicli.framework.CommandExecutor;
 import dev.costas.minicli.framework.HelpGenerator;
 import dev.costas.minicli.framework.Instantiator;
 import dev.costas.minicli.models.ApplicationParams;
+import dev.costas.minicli.models.Invocation;
 import org.reflections.Reflections;
 
 import java.io.OutputStream;
+import java.lang.reflect.Field;
 import java.util.List;
 
 /**
@@ -46,39 +50,127 @@ public class MinicliApplication {
 	 * @param clazz The package to scan for commands. It also scans subpackages.
 	 * @param args  The arguments to pass to the command.
 	 */
-	public void run(Class<?> clazz, String[] args) throws QuitException {
+	public void run(Class<?> clazz, String[] args) throws QuitException, IllegalAccessException {
 		var classes = getCommands(clazz.getPackageName());
-
-		var invocation = argumentParser.parse(args);
-
-		if (invocation.getCommand() == null || invocation.getCommand().isBlank() || invocation.getCommand().equals("help")) {
+		if (args.length == 0) {
 			this.helpGenerator.show(application, classes, outputStream);
 			return;
 		}
 
-		if (invocation.getCommand().equals("quit") || invocation.getCommand().equals("exit")) {
+		if (args[0].equals("help")) {
+			this.helpGenerator.show(application, classes, outputStream);
+			return;
+		}
+
+		if (args[0].equals("quit") || args[0].equals("q") || args[0].equals("exit")) {
 			throw new QuitException();
 		}
 
-		var commandClass = classes.stream()
+		var candidades = classes.stream()
 				.filter(c -> {
 					var name = c.getAnnotation(Command.class).name().toLowerCase();
 					var shortName = c.getAnnotation(Command.class).shortname().toLowerCase();
-					return name.equals(invocation.getCommand()) || shortName.equals(invocation.getCommand());
+					return name.equals(args[0]) || shortName.equals(args[0]);
 				})
-				.findFirst();
+				.toList();
 
-		if (commandClass.isEmpty()) {
-			// TODO: Show an error message or throw, since the command is not found because of the developer's mistake.
-			this.helpGenerator.show(application, classes, outputStream);
-		} else {
-			var instance = instantiator.getInstance(commandClass.get());
-			if (!(instance instanceof RunnableCommand)) {
-				throw new RuntimeException("Command class must implement RunnableCommand");
+		switch (candidades.size()) {
+			case 0 -> throw new RuntimeException("Command not found.");
+			case 1 -> {
+				var instance = instantiator.getInstance(candidades.get(0));
+
+				if (!(instance instanceof RunnableCommand)) {
+					throw new RuntimeException("Command class must implement RunnableCommand");
+				}
+
+				inflateInstance(instance, args);
+				var invocation = argumentParser.parse(args);
+				commandExecutor.execute((RunnableCommand) instance, invocation, outputStream);
 			}
-			commandExecutor.execute((RunnableCommand) instance, invocation, outputStream);
+			default -> throw new RuntimeException("Multiple commands with the same name found.");
+
+		}
+
+	}
+
+	/**
+	 * Receives a class instance and injects the parameters and flags from the given arguments.
+	 *
+	 * @param instance The instance to inject the parameters and flags.
+	 * @param args     The arguments to parse.
+	 */
+	private void inflateInstance(Object instance, String[] args) throws IllegalAccessException, NumberFormatException {
+		var invocation = argumentParser.parse(args);
+		var clazz = instance.getClass();
+
+		for (Field field : clazz.getDeclaredFields()) {
+			var flagAnnotation = field.getAnnotation(Flag.class);
+			if (flagAnnotation != null) {
+				inflateFlag(field, instance, invocation);
+				continue; // Skip the parameter injection since flags cannot be parameters.
+			}
+
+			var parameterAnnotation = field.getAnnotation(Parameter.class);
+			if (parameterAnnotation != null) {
+				inflateParameter(field, instance, invocation);
+			}
 		}
 	}
+
+	private void inflateFlag(Field field, Object instance, Invocation invocation) throws IllegalAccessException {
+		// Gets the annotation
+		var flag = field.getAnnotation(Flag.class);
+
+		// Must be a boolean, otherwise it's not a flag
+		if (field.getType() != boolean.class && field.getType() != Boolean.class) {
+			throw new RuntimeException("Flag " + field.getName() + " must be a boolean.");
+		}
+
+		// Gets the value of the flag with the long name
+		var value = invocation.getFlag(flag.name());
+
+		// If the value is null, gets the value of the flag with the short name (if it exists)
+		if (value == null) {
+			value = invocation.getFlag(flag.shortName());
+		}
+
+		// If the value is null, it means that the flag was not passed, use the default value
+		if (value == null) {
+			value = flag.defaultValue();
+		}
+
+		field.set(instance, value);
+	}
+
+	private void inflateParameter(Field field, Object instance, Invocation invocation) throws IllegalAccessException, NumberFormatException {
+		// Gets the annotation
+		var parameterAnnotation = field.getAnnotation(Parameter.class);
+		var value = invocation.getParameter(parameterAnnotation.name());
+
+		// If the value is null, it means that the parameter was not passed, try with the short name
+		if (value == null) {
+			value = invocation.getParameter(parameterAnnotation.shortName());
+		}
+
+		// If the value is null, it means that the parameter was not passed, use the default value and throw if required
+		if (value == null) {
+			if (parameterAnnotation.required()) {
+				throw new RuntimeException("Required parameter " + parameterAnnotation.name() + " not found.");
+			}
+			value = invocation.getParameter(parameterAnnotation.defaultValue());
+		}
+
+		// Try to parse the value to the correct type and set it to the field
+		switch (field.getType().getName()) {
+			case "java.lang.String" -> field.set(instance, value);
+			case "int", "java.lang.Integer" -> field.set(instance, Integer.parseInt(value));
+			case "long", "java.lang.Long" -> field.set(instance, Long.parseLong(value));
+			case "float", "java.lang.Float" -> field.set(instance, Float.parseFloat(value));
+			case "double", "java.lang.Double" -> field.set(instance, Double.parseDouble(value));
+			default -> throw new RuntimeException("Unsupported parameter type " + field.getType().getName());
+		}
+	}
+
 
 	/**
 	 * Gets all classes in classpath that are annotated with @Command
